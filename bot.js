a4a40a2514c58c4fb60a65d0032cb3572ff5ecba
Mell <mellkayrusson@gmail.com>
2020-04-15 15:02:51 +0100

    updated

--- bot.js
+++ bot.js
@@ -1,5 +1,10 @@
 const twit = require("twit");
-const config = require("./config.js");
+const config = {
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
+};
 
 const Twitter = new twit(config);
 
